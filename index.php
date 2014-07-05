b5c997ea60b01a5cf22f24e445c63dc2ad46b8fa
Dan <daniel.carl@live.com>
2014-07-05 08:43:25 +0100

    formatted properly isn more

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<button type="button" class="btn btn-info">Info</button>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Dodgy is amazing</title>
+    </head>
+    <body>
+        <header>Swag</header>
+        <button type="button" class="btn btn-info">Info</button>
+    </body>
+</html>
